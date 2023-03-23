04dff1b9928a8c23f44b7ffee80e68e4abc45f55
pawelstasiewicz <pst357357@gmail.com>
2023-03-23 19:27:22 +0100

    Create all functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,24 @@
-//Edit
 const editBtn = document.querySelectorAll('.editBtn');
+const editAreaBtn = document.querySelector('.editAreaBtn');
 const editArea = document.querySelector('.editArea');
-//Add task
 const taskInputValue = document.querySelector('.taskInputValue');
 const addTaskBtn = document.querySelector('.addTaskBtn');
-//Task area
 const taskArea = document.querySelector('.taskArea ul');
 const errorInfo = document.querySelector('.errorInfo');
+const editInput = document.querySelector('.editInput');
 let newTask;
+let editedTask;
+const editErrorr = document.querySelector('.editErrorr');
 
 const main = () => {
 	prapareDOMEvents();
 };
 
 const prapareDOMEvents = () => {
-	editBtn.forEach((btn) => {
-		btn.addEventListener('click', editFunction);
-	});
-
 	addTaskBtn.addEventListener('click', addTask);
+	taskArea.addEventListener('click', checkClick);
+	editAreaBtn.addEventListener('click', changeToDoTask);
+	taskInputValue.addEventListener('keyup', enterTask);
 };
 
 const addTask = () => {
@@ -26,7 +26,7 @@ const addTask = () => {
 		errorInfo.textContent = 'You must write something';
 		errorInfo.style.color = 'red';
 	} else if (taskInputValue.vlaue !== '') {
-		newTask = document.createElement('li')
+		newTask = document.createElement('li');
 		newTask.textContent = taskInputValue.value;
 		taskArea.append(newTask);
 
@@ -53,8 +53,37 @@ const createBtnArea = () => {
 	btnArea.append(btnEdit, btnDelete);
 };
 
-const editFunction = () => {
+const checkClick = (e) => {
+	if (e.target.matches('.editBtn')) {
+		editTask(e);
+	} else if (e.target.matches('.deleteBtn')) {
+		deleteTask(e);
+	}
+};
+
+const editTask = (e) => {
+	editedTask = e.target.closest('li');
+	editInput.value = editedTask.firstChild.textContent;
+	editErrorr.textContent = '';
 	editArea.classList.toggle('active');
+};
+
+const changeToDoTask = () => {
+	if (editInput.value !== '') {
+		editedTask.firstChild.textContent = editInput.value;
+	} else {
+		editErrorr.textContent = 'You must write something';
+	}
+};
+
+const deleteTask = (e) => {
+	e.target.closest('li').remove();
+};
+
+const enterTask = (e) => {
+	if (e.key === 'Enter') {
+		addTask();
+	}
 };
 
 document.addEventListener('DOMContentLoaded', main);
